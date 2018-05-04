032ab289c0bce9f1f9f48e1d830e16c334018448
RealityMachina <tomclancy1995@gmail.com>
2018-05-04 18:20:02 -0400

    Initial set up of better juggernaut.

---
+++ BetterJuggernaut/SprintAndPunch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Harmony;
+using BattleTech;
+
+namespace BetterJuggernaut
+{
+    public class SprintAndPunch
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("Battletech.realitymachina.BetterJuggernaut");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleTech.MechMeleeSequence), "GenerateMeleePath")]
+    public static class BattleTech_GenerateMeleePath_Patch
+    {
+        static bool Prefix(MechMeleeSequence __instance)
+        {
+            //no easy way to avoid incompatibilites here: we're completely overriding the logic so we can do what we need to do
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(BattleTech.MechMeleeSequence), "GenerateMeleePath")]
+    public static class BattleTech_GenerateMeleePath_Postfix
+    {
+        static void Postfix(MechMeleeSequence __instance)
+        {
+            Type meleeSeqType = typeof(MechMeleeSequence);
+            FieldInfo field1 = meleeSeqType.GetField("MoveIsCharge", BindingFlags.NonPublic | BindingFlags.Instance);
+            FieldInfo field2 = meleeSeqType.GetField("moveSequence", BindingFlags.NonPublic | BindingFlags.Instance);
+            ActorMovementSequence repSeq = new ActorMovementSequence(__instance.OwningMech)
+            {
+                IgnoreEndSmoothing = false,
+                meleeType = __instance.selectedMeleeType
+            };
+            // __instance.MoveIsCharge = false;
+            field1.SetValue(__instance, false); //this should set moveischarge to false?
+            __instance.OwningMech.Pathing.SetWalking();
+            __instance.OwningMech.Pathing.SetMeleeTarget(__instance.MeleeTarget as AbstractActor);
+            __instance.OwningMech.Pathing.SelectMeleeDest(__instance.DesiredMeleePosition);
+            __instance.OwningMech.Pathing.Update(__instance.MeleeTarget.CurrentPosition, false);
+            field2.SetValue(__instance, repSeq);
+    
+          //  __instance.moveSequence = new ActorMovementSequence(__instance.OwningMech);
+          //  __instance.moveSequence.IgnoreEndSmoothing = __instance.MoveIsCharge;
+         //   __instance.moveSequence.meleeType = __instance.selectedMeleeType;
+            __instance.OwningMech.Pathing.ClearMeleeTarget();
+        }
+    }
+}
